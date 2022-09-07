ee5ee063751d431846215464675acd58925636d6
sallen21 <simoneallen00@yahoo.com>
2022-09-07 11:09:45 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,99 +1,135 @@
 'use strict';
-const deferToConnect = require('defer-to-connect');
+const os = require('os');
+const tty = require('tty');
+const hasFlag = require('has-flag');
 
-module.exports = request => {
-	const timings = {
-		start: Date.now(),
-		socket: null,
-		lookup: null,
-		connect: null,
-		upload: null,
-		response: null,
-		end: null,
-		error: null,
-		phases: {
-			wait: null,
-			dns: null,
-			tcp: null,
-			request: null,
-			firstByte: null,
-			download: null,
-			total: null
+const {env} = process;
+
+let forceColor;
+if (hasFlag('no-color') ||
+	hasFlag('no-colors') ||
+	hasFlag('color=false') ||
+	hasFlag('color=never')) {
+	forceColor = 0;
+} else if (hasFlag('color') ||
+	hasFlag('colors') ||
+	hasFlag('color=true') ||
+	hasFlag('color=always')) {
+	forceColor = 1;
+}
+
+if ('FORCE_COLOR' in env) {
+	if (env.FORCE_COLOR === 'true') {
+		forceColor = 1;
+	} else if (env.FORCE_COLOR === 'false') {
+		forceColor = 0;
+	} else {
+		forceColor = env.FORCE_COLOR.length === 0 ? 1 : Math.min(parseInt(env.FORCE_COLOR, 10), 3);
+	}
+}
+
+function translateLevel(level) {
+	if (level === 0) {
+		return false;
+	}
+
+	return {
+		level,
+		hasBasic: true,
+		has256: level >= 2,
+		has16m: level >= 3
+	};
+}
+
+function supportsColor(haveStream, streamIsTTY) {
+	if (forceColor === 0) {
+		return 0;
+	}
+
+	if (hasFlag('color=16m') ||
+		hasFlag('color=full') ||
+		hasFlag('color=truecolor')) {
+		return 3;
+	}
+
+	if (hasFlag('color=256')) {
+		return 2;
+	}
+
+	if (haveStream && !streamIsTTY && forceColor === undefined) {
+		return 0;
+	}
+
+	const min = forceColor || 0;
+
+	if (env.TERM === 'dumb') {
+		return min;
+	}
+
+	if (process.platform === 'win32') {
+		// Windows 10 build 10586 is the first Windows release that supports 256 colors.
+		// Windows 10 build 14931 is the first release that supports 16m/TrueColor.
+		const osRelease = os.release().split('.');
+		if (
+			Number(osRelease[0]) >= 10 &&
+			Number(osRelease[2]) >= 10586
+		) {
+			return Number(osRelease[2]) >= 14931 ? 3 : 2;
 		}
-	};
 
-	const handleError = origin => {
-		const emit = origin.emit.bind(origin);
-		origin.emit = (event, ...args) => {
-			// Catches the `error` event
-			if (event === 'error') {
-				timings.error = Date.now();
-				timings.phases.total = timings.error - timings.start;
+		return 1;
+	}
 
-				origin.emit = emit;
-			}
-
-			// Saves the original behavior
-			return emit(event, ...args);
-		};
-	};
-
-	let uploadFinished = false;
-	const onUpload = () => {
-		timings.upload = Date.now();
-		timings.phases.request = timings.upload - timings.connect;
-	};
-
-	handleError(request);
-
-	request.once('socket', socket => {
-		timings.socket = Date.now();
-		timings.phases.wait = timings.socket - timings.start;
-
-		const lookupListener = () => {
-			timings.lookup = Date.now();
-			timings.phases.dns = timings.lookup - timings.socket;
-		};
-
-		socket.once('lookup', lookupListener);
-
-		deferToConnect(socket, () => {
-			timings.connect = Date.now();
-
-			if (timings.lookup === null) {
-				socket.removeListener('lookup', lookupListener);
-				timings.lookup = timings.connect;
-				timings.phases.dns = timings.lookup - timings.socket;
-			}
-
-			timings.phases.tcp = timings.connect - timings.lookup;
-
-			if (uploadFinished && !timings.upload) {
-				onUpload();
-			}
-		});
-	});
-
-	request.once('finish', () => {
-		uploadFinished = true;
-
-		if (timings.connect) {
-			onUpload();
+	if ('CI' in env) {
+		if (['TRAVIS', 'CIRCLECI', 'APPVEYOR', 'GITLAB_CI', 'GITHUB_ACTIONS', 'BUILDKITE'].some(sign => sign in env) || env.CI_NAME === 'codeship') {
+			return 1;
 		}
-	});
 
-	request.once('response', response => {
-		timings.response = Date.now();
-		timings.phases.firstByte = timings.response - timings.upload;
+		return min;
+	}
 
-		handleError(response);
+	if ('TEAMCITY_VERSION' in env) {
+		return /^(9\.(0*[1-9]\d*)\.|\d{2,}\.)/.test(env.TEAMCITY_VERSION) ? 1 : 0;
+	}
 
-		response.once('end', () => {
-			timings.end = Date.now();
-			timings.phases.download = timings.end - timings.response;
-			timings.phases.total = timings.end - timings.start;
-		});
-	});
+	if (env.COLORTERM === 'truecolor') {
+		return 3;
+	}
 
-	return timings;
+	if ('TERM_PROGRAM' in env) {
+		const version = parseInt((env.TERM_PROGRAM_VERSION || '').split('.')[0], 10);
+
+		switch (env.TERM_PROGRAM) {
+			case 'iTerm.app':
+				return version >= 3 ? 3 : 2;
+			case 'Apple_Terminal':
+				return 2;
+			// No default
+		}
+	}
+
+	if (/-256(color)?$/i.test(env.TERM)) {
+		return 2;
+	}
+
+	if (/^screen|^xterm|^vt100|^vt220|^rxvt|color|ansi|cygwin|linux/i.test(env.TERM)) {
+		return 1;
+	}
+
+	if ('COLORTERM' in env) {
+		return 1;
+	}
+
+	return min;
+}
+
+function getSupportLevel(stream) {
+	const level = supportsColor(stream, stream && stream.isTTY);
+	return translateLevel(level);
+}
+
+module.exports = {
+	supportsColor: getSupportLevel,
+	stdout: translateLevel(supportsColor(true, tty.isatty(1))),
+	stderr: translateLevel(supportsColor(true, tty.isatty(2)))
 };
