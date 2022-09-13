0e12acc4552f3c19eb1c45ac7414705a52c7edd6
sallen21 <simoneallen00@yahoo.com>
2022-09-12 23:55:59 -0400

    Add files via upload

--- app-routing.module.ts
+++ app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './components/about-us/about-us.component';
 import { HomeComponent } from './components/home/home.component';
 import { ListingsComponent } from './components/listings/listings.component';
 import { LoginComponent } from './components/login/login.component';
@@ -10,7 +11,8 @@ const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'signup', component: SignupComponent},
   {path: 'pets', component: ListingsComponent},
-  {path: 'home', component: HomeComponent}
+  {path: 'home', component: HomeComponent},
+  {path: 'about-us', component: AboutUsComponent}
 ];
 
 @NgModule({
